36de63ab115178f1df1097770f875f8e3779a9bb
Irakli <iraklichabiev7@gmail.com>
2025-10-22 16:13:52 +0400

    sec6

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 
 using API.DTOs;
 using API.Errors;
+using API.Helpers;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -30,12 +31,19 @@ public class ProductsController : BaseApiController
      }
 
      [HttpGet]
-     public async Task <ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+     public async Task <ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+     [FromQuery]ProductSpecParams productParams)
      {
-          var spec = new PeoductsWithTypesAndBrandsSpecification();
+          var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+          
+          var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+          
+          var totalItems = await _productsRepo.CountAsync(countSpec);
           var products = await _productsRepo.ListAsync(spec);
-          return Ok(_mapper
-               .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+          var data = _mapper
+               .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+          
+          return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
      }
      
      [HttpGet("{id}")]
@@ -43,7 +51,7 @@ public class ProductsController : BaseApiController
      [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
      public async Task <ActionResult<ProductToReturnDto>> GetProduct(int id)
      {
-          var spec =  new PeoductsWithTypesAndBrandsSpecification(id);
+          var spec =  new ProductsWithTypesAndBrandsSpecification(id);
 
           var product = await _productsRepo.GetEntityWithSpec(spec);
 
